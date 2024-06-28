3c8bcf0c5dad752f3e59bcdafde9ea8f1f673fdc
Mohammad Rangga Irghivya <mohammadranggairghivya@gmail.com>
2024-06-28 16:38:51 +0700

    first commit

---
+++ src/form/mahasiswa/FormMahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package form.mahasiswa;
+
+/**
+ *
+ * @author ranggairghi
+ */
+public class FormMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        formMahaiswa frhMhs = new formMahaiswa();
+        frhMhs.setVisible(true);
+        
+    }
+    
+}
